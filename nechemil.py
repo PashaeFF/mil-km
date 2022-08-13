a83e26fcd8096d6a28addda4e6c3ad36936fe370
Rafig Pashayev <pashayevler@gmail.com>
2022-08-14 00:38:29 +0400

    mil-km

---
+++ nechemil.py
@@ -0,0 +1,16 @@
+
+# y = 0.6214
+
+# x = int(input("Mile chevirmek istediyin km qeyd et: "))
+# z = x*y
+# print(z)
+
+
+
+km = 1.6214
+
+mil = float(input("Km'e chevirmek istediyin mili qeyd et: "))
+
+z = km*mil
+
+print(z)
